e8f0a82090b9b71d47e07cdb8ec7516df0936e0f
rdarbha <rama.h.darbha@gmail.com>
2017-02-20 14:54:00 -0500

    updating code for new functions

--- jinyang.py
+++ jinyang.py
@@ -78,12 +78,12 @@ def generate_slack_response(text, in_channel=True):
 
     return jsonify(response)
 
-# def return_quote(quote_option):
-#     return {
-#         1 : "Erlich Bachman, this is you as an old man. I'm ugly and I'm dead. Alone."
-#         2 : "Eric Bachman, is your refrigerator running? This is Mike Hunt."
-#         3 : "Eric Bachman, this is your mom, and you, you are not my baby. "
-#     }[quote_option]
+def return_quote(quote_option):
+    return {
+        1 : "Erlich Bachman, this is you as an old man. I'm ugly and I'm dead. Alone.",
+        2 : "Eric Bachman, is your refrigerator running? This is Mike Hunt.",
+        3 : "Eric Bachman, this is your mom, and you, you are not my baby. "
+    }[quote_option]
 
 @app.route('/', methods=["GET", "POST"])
 def index():
@@ -96,32 +96,32 @@ def test():
 @app.route('/jinyang', methods=["GET", "POST"])
 def jinyang():
 
+    quote_option = random.randint(1, 3)
+    output = return_quote(quote_option)
+    # quote_option = {
+    #     1 : "Erlich Bachman, this is you as an old man. I'm ugly and I'm dead. Alone.",
+    #     2 : "Eric Bachman, is your refrigerator running? This is Mike Hunt.",
+    #     3 : "Eric Bachman, this is your mom, and you, you are not my baby. "
+    # }
+
     if debug:
         print(request.form)
 
 #     try:
 #         # First parse the inbound slack message and get a simple dict
 #         slack_dict = parse_slack_message(request.form)
-
 #         output = slack_dict["text"]
-
 #     return generate_slack_response(output)
 
-    quote_option = random.randint(1, 3)
 
-    if quote_option == 1:
-        output = "Erlich Bachman, this is you as an old man. I'm ugly and I'm dead. Alone."
-    elif quote_option == 2:
-        output = "Eric Bachman, is your refrigerator running? This is Mike Hunt."
-    elif quote_option == 3:
-        output = "Eric Bachman, this is your mom, and you, you are not my baby. "
 
-    # output = return_quote(quote_option)
-    # quote_option = {
-    #     1 : "Erlich Bachman, this is you as an old man. I'm ugly and I'm dead. Alone.",
-    #     2 : "Eric Bachman, is your refrigerator running? This is Mike Hunt.",
-    #     3 : "Eric Bachman, this is your mom, and you, you are not my baby. "
-    # }
+    # if quote_option == 1:
+    #     output = "Erlich Bachman, this is you as an old man. I'm ugly and I'm dead. Alone."
+    # elif quote_option == 2:
+    #     output = "Eric Bachman, is your refrigerator running? This is Mike Hunt."
+    # elif quote_option == 3:
+    #     output = "Eric Bachman, this is your mom, and you, you are not my baby. "
+
 
     # output = quote_option[random.randint(1,3)]
 
